50a1f05da0f3f01b5125eb1822ad7d137cabd1bd
NazmulSuzon <nazmulsuzon100@gmail.com>
2023-03-21 05:43:46 +0600

    banner complete

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,77 @@
+import React from "react";
+import img1 from "../../../images/slider/slider-mainbg-01.jpg";
+import img2 from "../../../images/slider/slider-mainbg-02.jpg";
+import video from '../../../images/video-btn.png';
+import './Banner.css';
+import { Link } from "react-router-dom";
+
+const Banner = () => {
+  return (
+    <div className="banner" style={{height:'630px'}}>
+      <div className=" carousel max-w-[1500px] banner-image mx-auto pt-4">
+        <div id="slide1" style={{height:'700px'}} className="relative w-full carousel-item">
+          <img src={img1} alt="" className="w-full" />
+          <div className="centered">
+            <h1 className="text-6xl font-bold text-[#4B4D75]">The Best <span className="text-[#B07AB4]">PSY.D.</span> </h1><br />
+            <h1 className="text-6xl font-bold text-[#4B4D75]">Programs in</h1><br />
+            <h1 className="text-6xl font-bold text-[#4B4D75]">Clinical Psychology</h1><br />
+            <div className="flex pt-5">
+            <Link to="/appointment" className="btn border-0 text-white bg-[#B07AB4]">
+          Find My Program
+        </Link>
+        <img src={video} style={{width:'50px', height:'50px', marginLeft:'18px'}} alt="" />
+        <p className="font-bold place-self-center text-[#4B4D75]" style={{marginLeft:'18px'}}>WATCH VIDEO</p>
+            </div>
+          </div>
+          <div className="absolute flex flex-col transform -translate-y-1/2 place-items-end left-5 right-5 top-1/2">
+            <a
+              href="#slide4"
+              className="text-xl text-white hover:text-2xl btn-circle"
+            >
+              ⦿
+            </a>
+            <a
+              href="#slide2"
+              className="text-xl text-white hover:text-2xl btn-circle"
+            >
+              ⦿
+            </a>
+          </div>
+        </div>
+        <div id="slide2" className="relative w-full carousel-item">
+          <img src={img2} alt="" className="w-full" />
+          <div className="img2-text">
+          <p className="btn border-0 text-white bg-[#B07AB4]">
+          11 YEARS OF EXPERIENCE
+        </p>
+            <h1 className="text-6xl font-bold text-[#4B4D75]">We Provide</h1><br />
+            <h1 className="text-6xl font-bold text-[#4B4D75]"><span className="text-[#B07AB4]">Psychology Programs</span></h1>
+            <p className="text-xl pt-8 font-bold text-[#4B4D75]">Clinical Psychologist <span> • Unlimited Revisions</span> <span> • 24/7 support</span></p><br />
+            <Link to="/about" className="px-8 text-black bg-white btn hover:text-white hover:bg-[#5A5C82]">
+          About Us!
+        </Link>
+        <Link to="/services" className="btn px-8 border-0 ml-6 text-white bg-[#B07AB4]">
+          Our Services
+        </Link>
+          </div>
+          <div className="absolute flex flex-col transform -translate-y-1/2 place-items-end left-5 right-5 top-1/2">
+            <a
+              href="#slide1"
+              className="text-xl text-white hover:text-2xl btn-circle"
+            >
+              ⦿
+            </a>
+            <a
+              href="#slide3"
+              className="text-xl text-white hover:text-2xl btn-circle"
+            >
+              ⦿
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -80,8 +80,8 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
+    <div className="navbar px-32 bg-[#5A5C82]">
+      <div className=" navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
             <svg
@@ -101,18 +101,22 @@ const Navbar = () => {
           </label>
           <ul
             tabIndex={0}
-            className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            className="p-2 mt-3 text-black bg-white shadow menu menu-compact dropdown-content rounded-box w-52"
           >
             {menuItems}
           </ul>
         </div>
-        <Link to="/"><img src={logo} alt="" /></Link>
+        <Link to="/">
+          <img src={logo} alt="" />
+        </Link>
       </div>
-      <div className="hidden navbar-center lg:flex">
+      <div className="hidden text-white navbar-center lg:flex ">
         <ul className="px-1 menu menu-horizontal">{menuItems}</ul>
       </div>
       <div className="navbar-end">
-        <Link to="/appointment" className="btn">Appointment</Link>
+        <Link to="/appointment" className="text-white bg-[#5A5C82] border-white btn px-6">
+          Appointment!
+        </Link>
       </div>
     </div>
   );
