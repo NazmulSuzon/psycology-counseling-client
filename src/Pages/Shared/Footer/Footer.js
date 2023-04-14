2d24454f684c69df9522218d145cb91f444af928
NazmulSuzon <nazmulsuzon100@gmail.com>
2023-04-15 05:37:37 +0600

    footer continue

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -3,15 +3,15 @@ import email from "../../../images/mail.png";
 
 const Footer = () => {
   return (
-    <div className="bg-neutral">
-      <div className="container mx-auto">
-        <div className="flex">
+    <div className="mt-20 bg-neutral">
+      <div className="container pt-20 mx-auto">
+        <div className="flex items-center mb-8 border-2">
           <img src={email} style={{ width: "60px" }} alt="" />
-          <h5 className="text-xl font-bold text-white">
+          <h5 className="ml-8 text-xl font-bold text-white">
             Get Latest Updates Subscribe <br /> To Our Newsletter
           </h5>
-          <form class="w-full max-w-sm">
-            <div class="flex items-center border-b border-teal-500 py-2">
+          <form class=" ml-8" style={{backgroundColor:'rgba(0, 0, 0, 0.03)', width:'500px',}}>
+            <div class="flex items-center border-b border-teal-500 py-2 ">
               <input
                 class="appearance-none bg-transparent border-none w-full text-white mr-3 py-1 font-bold px-2 leading-tight focus:outline-none"
                 type="text"
@@ -19,7 +19,7 @@ const Footer = () => {
                 aria-label="Your Email"
               />
               <button
-                class="flex-shrink-0 bg-primary hover:bg-neutral  text-sm  text-white py-1 px-2 rounded"
+                class="flex-shrink-0 bg-primary hover:bg-neutral font-bold text-sm  text-white px-5 py-3 rounded"
                 type="button"
               >
                 SUBSCRIBE NOW!
@@ -27,6 +27,7 @@ const Footer = () => {
             </div>
           </form>
         </div>
+        
       </div>
     </div>
   );
